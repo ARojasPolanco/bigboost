330f4efb56a35e44da23e728538f4e8986ce9dde
ARojasPolanco <advbrrop23@gmail.com>
2024-05-01 17:31:05 -0300

    fix: change grid in racerList component

--- src/layout/RacersList.jsx
+++ src/layout/RacersList.jsx
@@ -2,7 +2,7 @@ import RacersCard from "./RacersCard"
 
 const RacersList = ({ racers }) => {
     return (
-        <section className="grid grid-cols-1 sm:grid-cols-2 gap-6 items-center">
+        <section className="grid grid-cols-1 sm:grid-cols-3 gap-6 items-center">
             {
                 racers.map((racer) => <RacersCard key={racer.id} racer={racer} />)
             }
